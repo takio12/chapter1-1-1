e09ac83e54d2c4075b9204e006d5dcfc82e1316a
TAKIO-PC\takio <taka21centry@gmail.com>
2017-11-28 01:00:55 +0900

    1-1-1終わり

---
+++ CSharp_Chapter_1_1_1/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doradora
+{
+
+    public class Product
+    {
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public int GetTax()
+        {
+            return (int)(Price * 0.08);
+        }
+    }
+}
--- CSharp_Chapter_1_1_1/Program.cs
+++ CSharp_Chapter_1_1_1/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using doradora;
+/// <summary>
+/// </summary>
 namespace CSharp_Chapter_1_1_1
 {
     class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+           
+            Product dorayaki = new Product();
+            dorayaki.Code = 98;
+            dorayaki.Price = 210;
+            Console.WriteLine(dorayaki.GetTax());
+            Console.ReadKey();
         }
     }
+
 }
